f31b931330b04452f2e9d58ac37c56d54c9dffc8
CuteLittleGoat <taryf@o2.pl>
2026-06-17 07:45:24 +0200

    Load shared Firebase preview config in DataSlate

--- DataSlate/config/firebase-config.js
+++ DataSlate/config/firebase-config.js
@@ -1,3 +1,13 @@
 // config/firebase-config.js
 // DEMO/PREVIEW: final Firebase values are supplied by ../shared/firebase-config.js.
-window.firebaseConfig = window.WG_FIREBASE_CONFIG || window.firebaseConfig || {};
+(function loadSharedPreviewFirebaseConfig(){
+  if (!window.WG_FIREBASE_CONFIG) {
+    var request = new XMLHttpRequest();
+    request.open("GET", "../shared/firebase-config.js", false);
+    request.send(null);
+    if (request.status === 0 || (request.status >= 200 && request.status < 300)) {
+      (0, eval)(request.responseText);
+    }
+  }
+  window.firebaseConfig = window.firebaseConfig || window.WG_FIREBASE_CONFIG || {};
+})();
