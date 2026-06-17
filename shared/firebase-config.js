c88303876108f68536da44271c064550f9452ab8
CuteLittleGoat <taryf@o2.pl>
2026-06-17 07:41:12 +0200

    Configure shared Firebase preview access

--- shared/firebase-config.js
+++ shared/firebase-config.js
@@ -1,15 +1,9 @@
 // --- Konfiguracja Firebase dla prywatnych danych DataVault / Firebase config for private DataVault data ---
-// Ten plik NIE zawiera hasła. / This file does NOT contain the password.
-// Hasło użytkownik wpisuje w formularzu aplikacji. / Password is entered by the user in the app form.
-// Web firebaseConfig nie jest hasłem; bezpieczeństwo zapewniają Firebase Auth i reguły RTDB. / Web firebaseConfig is not a password; security is provided by Firebase Auth and RTDB rules.
-window.WG_FIREBASE_CONFIG = {
-  apiKey: "INSERT_YOUR_API_KEY",
-  authDomain: "INSERT_YOUR_AUTH_DOMAIN",
-  databaseURL: "INSERT_YOUR_DATABASE_URL",
-  projectId: "INSERT_YOUR_PROJECT_ID",
-  storageBucket: "INSERT_YOUR_STORAGE_BUCKET",
-  messagingSenderId: "INSERT_YOUR_MESSAGING_SENDER_ID",
-  appId: "INSERT_YOUR_APP_ID"
-};
+// Ten plik podłącza DEMO/PREVIEW do tego samego projektu Firebase, którego używa rpg-dataslate-relay.
+// This file connects DEMO/PREVIEW to the same Firebase project used by rpg-dataslate-relay.
+// Hasło nie jest przechowywane w tym pliku. / The password is not stored in this file.
 
-window.WG_DATA_ACCESS_EMAIL = "INSERT_YOUR_TECHNICAL_USER_EMAIL";
+(function loadRelayPreviewFirebaseConfig(){
+  document.write('<script src="https://cutelittlegoat.github.io/rpg-dataslate-relay/DataSlate/config/firebase-config.js"><\/script>');
+  document.write('<script>window.WG_FIREBASE_CONFIG = Object.assign({}, window.firebaseConfig || {}, { databaseURL: "https://rpg-dataslate-relay-default-rtdb.europe-west1.firebasedatabase.app/" }); window.WG_DATA_ACCESS_EMAIL = "youhavebeenrickrolledbyme@gmail.com";<\/script>');
+})();
